f740525efbbe508d5da94bbded58be3c82b351b4
Tushar Verma <84769397+tusharverma20@users.noreply.github.com>
2022-12-20 10:50:42 +0530

    Create A1_5_FormattingOutput.java

---
+++ A1_5_FormattingOutput.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++)
+            {
+                String s1=sc.next();
+                int x=sc.nextInt();
+                System.out.printf("%-15s%03d%n",s1,x);
+            }
+            System.out.println("================================");
+
+    }
+}
+
+
+/*
+
+Input:
+
+java 100
+cpp 65
+python 50
+
+Output:
+================================
+java           100 
+cpp            065 
+python         050 
+================================
+
+*/
