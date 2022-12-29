321ade41d874a61e6a507c37ddd4fc55d18e7d42
Tushar Verma <84769397+tusharverma20@users.noreply.github.com>
2022-12-29 16:07:28 +0530

    Update A3_7_PermuteArray.java

--- A3_7_PermuteArray.java
+++ A3_7_PermuteArray.java
@@ -1,1 +1,28 @@
 
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+   int arr[]={1,2,3,4,5,6,7};
+   int n= arr.length;
+   
+   System.out.print("Original Array: ");
+   for(int i =0;i<n;i++)
+        System.out.print(arr[i]+" ");
+   
+        for(int i =0;i<1;i++){
+            for(int j =0;j<n-1;j++){
+            int temp = arr[j+1];
+            arr[j+1]=arr[j];
+            arr[j]=temp;
+        }
+   }
+   System.out.print("\nPermuted Array: ");
+        for(int i =0;i<n;i++){
+        System.out.print(arr[i]+" ");
+        
+        
+         }
+    }
+}
