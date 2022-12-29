f28da42eb124f826f2e171d705628e48def88b80
Tushar Verma <84769397+tusharverma20@users.noreply.github.com>
2022-12-29 17:18:17 +0530

    Update A3_9_PrintLeader.java

--- A3_9_PrintLeader.java
+++ A3_9_PrintLeader.java
@@ -1,1 +1,24 @@
 
+import java.util.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        
+   int arr[]={17,4,3,5,2};
+   int n= arr.length;
+int k=1;
+int[] arr2 = new int[20];
+
+        for(int i =0;i<n;i++){
+            int j;
+            for(j=i+1;j<n;j++){
+                if(arr[i]<=arr[j]){
+                    break;
+                }
+             
+            }
+          if (j == n) 
+                System.out.print(arr[i] + " ");
+        }
+    }
+}
