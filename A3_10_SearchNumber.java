e0dfc7e01f231d86ad01312ee6655bfd0009001a
Tushar Verma <84769397+tusharverma20@users.noreply.github.com>
2022-12-29 17:19:38 +0530

    Update A3_10_SearchNumber.java

--- A3_10_SearchNumber.java
+++ A3_10_SearchNumber.java
@@ -1,1 +1,26 @@
 
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int size=sc.nextInt();
+        int numbers[] = new int[size];   
+        for(int i=0;i<size;i++){
+          numbers[i]= sc.nextInt();
+        }
+        int x=sc.nextInt();
+        for(int i=0;i<numbers.length ;i++){
+            if(numbers[i] == x){
+            System.out.println(i);
+            }
+        }
+    }
+}
+
+        
+        
+    
